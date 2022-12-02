20a0c22461845515da1f58fb55f8b46eb550e9dc
Camilo Fernández <53354795+C-Fer@users.noreply.github.com>
2022-12-02 11:12:21 -0500

    Update Question A.py

--- Question A.py
+++ Question A.py
@@ -1,21 +1,22 @@
 
 import random
-
+#function definition
 def is_overlapping(line_A, line_B):
-
+    #Cases where there is no overlapping return false
     if line_A[0] > line_B[1]:
         return False
 
     elif line_A[1] < line_B[0]:
         return False
     
-    return True
+    return True #if overlapping exists it will return true
 
-i = 0
-limit = 15
+i = 0 #counter variable
 
-while i < 100:
+limit = 15 #to generate numbers between -15 and 15
 
+while i < 20: #to generate 20 examples
+    #generate random lines between [-15,15]
     line_A = (random.randint(-limit,limit),random.randint(-limit,limit))
 
     line_B = (random.randint(-limit,limit),random.randint(-limit,limit))
